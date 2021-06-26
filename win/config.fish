9e2530f13567f097dc27115556fbd7d354f75b49
ybw <2756456886@qq.com>
2021-06-26 21:10:03 +0800

    lazy commit

--- win/config.fish
+++ win/config.fish
@@ -2,16 +2,9 @@ function nvm
       bass source ~/.nvm/nvm.sh ';' nvm $argv
   end
 
-function desk
-    set cyg (ls / | grep cygdrive)
-    set wsl (ls / | grep mnt)
-    [ ! $cyg ] && cd /$wsl/c/Users/27564/Desktop
-    [ ! $wsl ] && cd /$cyg/c/Users/27564/Desktop
-  end
-
-function user
-        set cyg (ls / | grep cygdrive)
-    set wsl (ls / | grep mnt)
-    [ ! $cyg ] && cd /$wsl/c/Users/27564
-    [ ! $wsl ] && cd /$cyg/c/Users/27564
-  end
+switch (uname)
+case Linux
+    set desk /mnt/c/Users/27564/Desktop
+case CYGWIN_NT-10.0
+    set desk /cygdrive/c/Users/27564/Desktop
+end
